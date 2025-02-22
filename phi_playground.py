08020a70f1f46082239aa7cdd5fb342e132da5ae
Prajwal N <118053725+Prajwalng2@users.noreply.github.com>
2025-02-22 18:09:26 +0530

    Create phi_playground.py

---
+++ phi_playground.py
@@ -0,0 +1,46 @@
+
+from phi.agent import Agent
+import phi.api
+from phi.tools.yfinance import YFinanceTools
+from phi.tools.duckduckgo import DuckDuckGo
+from dotenv import load_dotenv
+from phi.model.groq import Groq
+
+import os
+import phi
+from phi.playground import Playground, serve_playground_app
+# Load environment variables from .env file
+load_dotenv()
+
+phi.api=os.getenv("PHI_API_KEY")
+
+## web search agent
+web_search_agent=Agent(
+    name="Web Search Agent",
+    role="Will Search the web for the info and data",
+    model=Groq(id="llama-3.3-70b-versatile"),
+    tools=[DuckDuckGo()],
+    instructions=["Alway include sources"],
+    show_tools_calls=True,
+    markdown=True,
+
+)
+
+## Financial agent
+finance_agent=Agent(
+    name="AI Finance Agent",
+    model=Groq(id="llama-3.3-70b-versatile"),
+    tools=[
+        YFinanceTools(stock_price=True,company_info=True,income_statements=True,key_financial_ratios=True,technical_indicators=True,historical_prices=True, analyst_recommendations=True, stock_fundamentals=True,
+                      company_news=True),
+    ],
+    instructions=["Use tables to display the data"],
+    show_tool_calls=True,
+    markdown=True,
+
+)
+
+app=Playground(agents=[finance_agent,web_search_agent]).get_app()
+
+if __name__=="__main__":
+    serve_playground_app("playground:app",reload=True)
